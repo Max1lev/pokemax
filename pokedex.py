377f4638e0804faee1f786061c44202b6ebf3957
Max1lev <maximelev31@gmail.com>
2026-09-21 18:02:55 +0200

    Update pokedex.py
    add pokémons 7-9

--- pokedex.py
+++ pokedex.py
@@ -7,4 +7,7 @@ POKEDEX = [
     Pokemon("Salamèche", ["feu"], 60),
     Pokemon("Reptincel", ["feu"], 110),
     Pokemon("Dracaufeu", ["feu", "vol"], 170),
+    Pokemon("Carapuce", ["eau"], 51),
+    Pokemon("Carabaffe", ["eau"], 100),
+    Pokemon("Tortank", ["eau"], 160),
 ]
